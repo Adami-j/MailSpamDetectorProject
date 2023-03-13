4a5466f5f1cccbc558727b5135721c43ab488d85
juju <julien.adami@etu.iut-tlse3.fr>
2023-03-13 19:45:41 +0100

    Mise à jour des dépendances et ajout de l'export du package tests dans le module-info.java

---
+++ src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/tests/UserModelTests.java
@@ -0,0 +1,14 @@
+package fr.til.projetfilrouge.mailspamdetectorproject.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class UserModelTests {
+
+    @Test
+    public void test(){
+        assertEquals(1,1);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module fr.til.projetfilrouge.mailspamdetectorproject {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires junit;
 
     opens fr.til.projetfilrouge.mailspamdetectorproject to javafx.fxml;
     exports fr.til.projetfilrouge.mailspamdetectorproject;
+    exports fr.til.projetfilrouge.mailspamdetectorproject.tests;
 }
