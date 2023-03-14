f6174a189421d90561e33271beba536596a5ecb0
juju <adamij@3il.fr>
2023-03-14 11:30:56 +0100

    Modification de la classe connexion service et ajout du test de connexion

--- src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/Tests/ConnexionControllerTest.java
+++ src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/Tests/ConnexionControllerTest.java
@@ -4,23 +4,21 @@ import fr.til.projetfilrouge.mailspamdetectorproject.Controller.ConnexionControl
 import fr.til.projetfilrouge.mailspamdetectorproject.Model.UserModel;
 import org.junit.Test;
 
+import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class ConnexionControllerTest {
 
     @Test
-    public void testConnexionControllerTest() throws NoSuchProviderException {
+    public void testConnexionControllerTest() throws MessagingException {
        assertEquals(1,1);
         UserModel userModel = UserModel.getInstance();
-        userModel.setLogin("");
-        userModel.setPassword("");
-       ConnexionController connexionController = ConnexionController.getInstance(userModel);
-       System.out.println( connexionController.isConnected());
+        userModel.setLogin("equipedetectorspmtest@outlook.fr");
+        userModel.setPassword("SpamTestBaiern12..");
+        System.out.println(userModel.getLogin()+userModel.getPassword());
 
+        ConnexionController connexionController = ConnexionController.getInstance(userModel);
     }
-
-
 }
