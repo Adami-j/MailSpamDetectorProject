c6959b36d67993e6712db0a96b22a7af986ed0db
Mateo Isabey <mateoisabey@gmail.com>
2023-03-14 10:57:24 +0100

    Ecriture JavaDoc readerCSV

--- src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/utils/readerCSV.java
+++ src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/utils/readerCSV.java
@@ -5,16 +5,22 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class readerCSV {
+
+    /**
+     * récupere la fonction un HashMap de tout les mots du CSV qui contient les termes pour la classification bayésienne
+     * @param filePath
+     * @return HashMap de String
+     */
     public static HashMap<String, String> readCSV(String filePath) {
         HashMap<String, String> wordCountMap = new HashMap<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] words = line.split(",");
+                String[] words = line.split(",");//récupère le mot, chaque "mot" est séparé par une virgule
                 for (String word : words) {
-                    word = word.trim();
-                    wordCountMap.put(word, word);
+                    word = word.trim(); //enlever tous les espaces qui peuvent poser un problème
+                    wordCountMap.put(word, word);//ajout du mot dans le HashMap
                 }
             }
         } catch (IOException e) {
