4adc9b08f5ae61363bf14a2f708ae016559706ad
Julien ADAMI <adamij@3il.fr>
2023-03-15 10:49:21 +0100

    Mise en forme et modification du controller des mails

--- src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/Controller/VisualisationMailController.java
+++ src/main/java/fr/til/projetfilrouge/mailspamdetectorproject/Controller/VisualisationMailController.java
@@ -1,11 +1,24 @@
 package fr.til.projetfilrouge.mailspamdetectorproject.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import javax.mail.Message;
 
 public class VisualisationMailController {
+
+    @FXML
+    private ListView<Message> nonSpamListView;
+
+    @FXML
+    private ListView<Message> spamListView;
+
     public void moveToNonSpam(ActionEvent actionEvent) {
+        
     }
 
     public void moveToSpam(ActionEvent actionEvent) {
+
     }
 }
